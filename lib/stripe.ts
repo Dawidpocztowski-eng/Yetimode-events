975e67daab197bdd4f22674f9900d54f0c1668c5
Dawid Pocztowski <dawidpocztowski@MacBook-Pro-Dawid.local>
2026-04-25 23:06:31 +0200

    fix: stripe api version compatible with stripe v16

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-04-22.dahlia',
+  apiVersion: '2024-06-20',
 })
 
 export const PLANS = {
